7f1b87a407a1cd6b4de3d221710590ad5bde118c
ferbass <ferbass@gmail.com>
2012-10-18 17:08:32 -0300

    adicionando modelo para eventos

---
+++ devinsantos/Models/DSEvent.h
@@ -0,0 +1,17 @@
+//
+//  DSEvent.h
+//  devinsantos
+//
+//  Created by Fernando Bass on 10/18/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSEvent : NSObject
+@property (strong) NSString *address;
+@property (strong) NSString *date;
+@property (strong) NSString *description;
+@property (strong) NSString *name;
+@property (strong) NSString *place;
+@end
---
+++ devinsantos/Models/DSEvent.m
@@ -0,0 +1,13 @@
+//
+//  DSEvent.m
+//  devinsantos
+//
+//  Created by Fernando Bass on 10/18/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import "DSEvent.h"
+
+@implementation DSEvent
+
+@end

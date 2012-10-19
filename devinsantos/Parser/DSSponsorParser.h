915391fe42da37705d2f98eaf4eae09c1ba2b7a6
Fernando Bass <ferbass@gmail.com>
2012-10-19 18:22:11 -0300

    parser para patrocinador

--- devinsantos/Parser/DSSponsorParser.h
+++ devinsantos/Parser/DSSponsorParser.h
@@ -9,5 +9,6 @@
 #import <Foundation/Foundation.h>
 
 @interface DSSponsorParser : NSObject
++ (NSArray *)parseSponsorWithJSON:(NSArray *)JSON;
 
 @end
--- devinsantos/Parser/DSSponsorParser.m
+++ devinsantos/Parser/DSSponsorParser.m
@@ -7,7 +7,25 @@
 //
 
 #import "DSSponsorParser.h"
-
+#import "DSSponsor.h"
 @implementation DSSponsorParser
+
++ (NSArray *)parseSponsorWithJSON:(NSArray *)JSON
+{
+    NSMutableArray *sponsors = [[NSMutableArray alloc] init];
+    
+    for (NSDictionary *_sponsor in JSON) {
+        
+        DSSponsor *sponsor = [[DSSponsor alloc] init];
+        sponsor.name = [_sponsor valueForKey:@"name"];
+        sponsor.logo = [_sponsor valueForKey:@"logo_url"];
+        sponsor.type = [_sponsor valueForKey:@"sponsorship_type"];
+        
+        [sponsors addObject:sponsor];
+        sponsor = nil;
+    }
+    
+    return sponsors;
+}
 
 @end

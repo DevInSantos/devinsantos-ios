f2b74b0d00c4ac467b52bb8a1c1826f9567c9475
Fernando Bass <ferbass@gmail.com>
2012-10-19 18:22:01 -0300

    parser para patrocinador

---
+++ devinsantos/Parser/DSSponsorParser.h
@@ -0,0 +1,13 @@
+//
+//  DSSponsorParser.h
+//  devinsantos
+//
+//  Created by Fernando Ribeiro on 10/19/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSSponsorParser : NSObject
+
+@end
---
+++ devinsantos/Parser/DSSponsorParser.m
@@ -0,0 +1,13 @@
+//
+//  DSSponsorParser.m
+//  devinsantos
+//
+//  Created by Fernando Ribeiro on 10/19/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import "DSSponsorParser.h"
+
+@implementation DSSponsorParser
+
+@end

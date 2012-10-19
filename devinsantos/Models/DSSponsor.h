01b4841429abec10a301176706f182fadb8ac37a
Fernando Bass <ferbass@gmail.com>
2012-10-19 18:18:52 -0300

    modelo para patrocinadores

---
+++ devinsantos/Models/DSSponsor.h
@@ -0,0 +1,15 @@
+//
+//  DSSponsor.h
+//  devinsantos
+//
+//  Created by Fernando Ribeiro on 10/19/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSSponsor : NSObject
+@property (strong) NSString *name;
+@property (strong) NSString *logo;
+@property (strong) NSString *type;
+@end
---
+++ devinsantos/Models/DSSponsor.m
@@ -0,0 +1,13 @@
+//
+//  DSSponsor.m
+//  devinsantos
+//
+//  Created by Fernando Ribeiro on 10/19/12.
+//  Copyright (c) 2012 Fernando Bass. All rights reserved.
+//
+
+#import "DSSponsor.h"
+
+@implementation DSSponsor
+
+@end
